7adf642f622cc03b3d6a1bbed136c4290b21fb31
Baiyuan Wang <bywang@thoughtworks.com>
2019-03-29 18:11:50 +0800

    auto accept new friend

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import os
 from wxpy import *
 
 env_dist = os.environ
-bot = Bot(cache_path=True, console_qr=1)
+bot = Bot(cache_path=True, console_qr=2)
 tuling = Tuling(api_key=env_dist.get('Tuling_API_KEY'))
-
-# 使用图灵机器人自动与指定好友聊天
 
 @bot.register(Friend, TEXT)
 def reply_my_friend(msg):
@@ -18,5 +16,9 @@ def reply_my_group(msg):
     if msg.is_at:
         tuling.do_reply(msg)
     print(msg)
+
+@bot.register(msg_types=FRIENDS)
+def auto_accept_friends(msg):
+    new_friend = msg.card.accept()
 
 embed()
