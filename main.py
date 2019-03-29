ac8fffa2b5b3bb52628da7909460925ed66de34b
Baiyuan Wang <bywang@thoughtworks.com>
2019-03-29 17:27:53 +0800

    integrate with Tuling

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import os
 from wxpy import *
-bot = Bot(cache_path=True, console_qr=2)
+
+env_dist = os.environ
+bot = Bot(cache_path=True, console_qr=1)
+tuling = Tuling(api_key=env_dist.get('Tuling_API_KEY'))
+
+# 使用图灵机器人自动与指定好友聊天
+
+@bot.register(Friend, TEXT)
+def reply_my_friend(msg):
+    tuling.do_reply(msg)
+    print(msg)
+
+
+@bot.register(Group, TEXT)
+def reply_my_group(msg):
+    if msg.is_at:
+        tuling.do_reply(msg)
+    print(msg)
+
 embed()
